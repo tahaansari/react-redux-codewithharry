b397d0c952623dd530be7272cb487914d0415b1e
Taha Ansari <taha.m.ansari@gmail.com>
2024-07-21 15:46:32 +0300

    wip

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+import Nav from './Nav'
+import Shop from "./Shop"
+
+function App() {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <Nav></Nav>
+      <Shop></Shop>
+    </>
+  )
+}
+
+export default App
